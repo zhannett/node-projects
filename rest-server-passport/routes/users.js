78c16de5c8cd65dbaa9b52361e32e6f1db49c66c
zhannett <janet@hubub.com>
2017-03-20 14:46:20 -0400

    Added HTTPS and secure communication

--- rest-server-passport/routes/users.js
+++ rest-server-passport/routes/users.js
@@ -14,7 +14,7 @@ var passport = require('passport');
 router.get('/', Verify.verifyOrdinaryUser, Verify.verifyAdmin, function(req, res, next) {
         User.find({}, function (err, user) {
             if (err) {
-                res.send('respond with a resource');
+                throw err;
             }
             res.json(user);
         });
@@ -35,8 +35,16 @@ router.post('/register', function(req, res) {
             if (err) {
                 return res.status(500).json({err: err});
             }
-            passport.authenticate('local')(req, res, function () {
-                return res.status(200).json({status: 'Registration Successful!'});
+            if(req.body.firstname) {
+                user.firstname = req.body.firstname;
+            }
+            if(req.body.lastname) {
+                user.lastname = req.body.lastname;
+            }
+            user.save(function(err,user) {
+                passport.authenticate('local')(req, res, function () {
+                    return res.status(200).json({status: 'Registration Successful!'});
+                });
             });
         });
 });
@@ -75,4 +83,6 @@ router.get('/logout', function(req, res) {
 });
 
 module.exports = router;
+
+
 
